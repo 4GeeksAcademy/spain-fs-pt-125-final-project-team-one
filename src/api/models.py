07d3132390933a74bcae73e1b25453eb83063672
IkerMoBu <92671501+Ikerichu@users.noreply.github.com>
2026-02-09 18:52:27 +0000

    Modelo Portfolio

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Date
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -9,11 +9,26 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    portfolio: Mapped[Portfolio]=relationship(back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Portfolio(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped[User]=relationship(back_populates="portfolio")
+    product: Mapped[int]=mapped_column(nullable=False)
+    date: Mapped[Date] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return{
+            "id":self.id,
+            "user_id":self.id,
+            "product":self.product,
+            "date":self.date
         }
