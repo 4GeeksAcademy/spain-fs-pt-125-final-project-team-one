70ca7e03be64f2e208279f840bea1410f76941c9
Miguel Angel Lopez Hermida <lacarricanta@hotmail.com>
2026-02-09 20:23:09 +0000

    Commit 09-02-2026

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from sqlalchemy import select
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +14,16 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route("/login", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+    user = db.session.execute(select(User).where(
+        User.email == email, User.password == password)).scalar_one_or_none()
+    if user is None:
+        return jsonify({"msg": "Bad username or password"}), 401
+    if not user.is_active:
+        return jsonify({"msg": "Usuario inactivo"}), 403
+    access_token = create_access_token(identity=str(user.id))
+    return jsonify({"token": access_token, "user_id": user.id})
