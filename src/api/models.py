ac6246c0699adea37bdd6787fb06dd0c24acfe24
IkerMoBu <92671501+Ikerichu@users.noreply.github.com>
2026-02-11 21:15:07 +0100

    Increase password field length to 300 characters

--- src/api/models.py
+++ src/api/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, unique=True)
     email: Mapped[str] = mapped_column(
         String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(String(300), nullable=False)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
     last_name: Mapped[str] = mapped_column(String(120), nullable=False)
     favorites_array = Column(ARRAY(Integer), nullable=False, default=list)
