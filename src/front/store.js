c2b21fa054f6b01c8ce8834dda57a44bbd1ce5b1
IkerMoBu <92671501+Ikerichu@users.noreply.github.com>
2026-02-13 19:46:48 +0000

    Llama a api y almacenamiento en store.api

--- src/front/store.js
+++ src/front/store.js
@@ -1,38 +1,63 @@
-export const initialStore=()=>{
-  return{
+export const initialStore = () => {
+  return {
     message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
-  }
-}
+    api: {
+      data: null,
+      loading: false,
+      error: null,
+    },
+  };
+};
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'set_hello':
+  switch (action.type) {
+    case "set_hello":
       return {
         ...store,
-        message: action.payload
+        message: action.payload,
       };
-      
-    case 'add_task':
 
-      const { id,  color } = action.payload
+    case "add_task":
+      const { id, color } = action.payload;
 
       return {
         ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+        todos: store.todos.map((todo) =>
+          todo.id === id ? { ...todo, background: color } : todo,
+        ),
       };
+
+    case "API_LOADING":
+      return {
+        ...store,
+        api: {
+          ...store.api,
+          loading: true,
+          error: null,
+        },
+      };
+
+    case "API_SUCCESS":
+      return {
+        ...store,
+        api: {
+          data: action.payload,
+          loading: false,
+          error: null,
+        },
+      };
+
+    case "API_ERROR":
+      return {
+        ...store,
+        api: {
+          ...store.api,
+          loading: false,
+          error: action.payload,
+        },
+      };
+
     default:
-      throw Error('Unknown action.');
-  }    
+      throw Error("Unknown action.");
+  }
 }
