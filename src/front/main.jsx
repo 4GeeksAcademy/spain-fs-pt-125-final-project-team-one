6f6d32c16617c53cfbd2a3dbe7083e8bd1b626fb
IkerMoBu <92671501+Ikerichu@users.noreply.github.com>
2026-02-16 19:38:52 +0000

    Eficiencia de archivos

--- src/front/main.jsx
+++ src/front/main.jsx
@@ -5,7 +5,7 @@ import { RouterProvider } from "react-router-dom";  // Import RouterProvider to 
 import { router } from "./routes";  // Import the router configuration
 import { StoreProvider } from './hooks/useGlobalReducer';  // Import the StoreProvider for global state management
 import { BackendURL } from './components/BackendURL';
-import API_Call from "./components/API_Call";
+
 
 const Main = () => {
     if(! import.meta.env.VITE_BACKEND_URL ||  import.meta.env.VITE_BACKEND_URL == "") return (
@@ -18,7 +18,7 @@ const Main = () => {
             {/* Provide global state to all components */}
             
             <StoreProvider> 
-                <API_Call />
+                
                 {/* Set up routing for the application */} 
                 <RouterProvider router={router}>
                 </RouterProvider>
